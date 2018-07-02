5d5bbcf24aa806ef2daea972855b4d87efbf1e26
Elle Faraday <faraday.elle@gmail.com>
2018-07-02 16:48:25 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
 
   def make_title_case
     self.title = self.title.titlecase
+  end
 
   private
 
